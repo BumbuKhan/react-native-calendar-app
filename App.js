29960a03e2faf2438af66f1d73770839dde40e1d
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 16:34:47 +0400

    Added react-navigarion package

--- App.js
+++ App.js
@@ -5,44 +5,11 @@ import Tabs from 'react-native-tabs'
 class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {curPage: 'third'}
   }
 
   render() {
     return (
       <View style={style.container}>
-        <Tabs
-            selected={this.state.curPage}
-            style={{backgroundColor: 'white'}}
-            onSelect={el => this.setState({curPage: el.props.name})}
-        >
-          <View name='first' style={style.tabItem}>
-            <Image source={require('./img/home.png')}/>
-            <Text>Home</Text>
-          </View>
-
-          <View name='second' style={style.tabItem}>
-            <Image source={require('./img/users.png')}/>
-            <Text>Contacts</Text>
-          </View>
-
-          <View name='third' style={style.tabItem}>
-            <Image source={require('./img/calendar.png')}/>
-            <Text>Calendar</Text>
-          </View>
-
-          <View name='fourth' style={style.tabItem}>
-            <Image source={require('./img/chat.png')}/>
-            <Text>Inbox</Text>
-          </View>
-
-          <View name='fifth' style={style.tabItem}>
-            <Image source={require('./img/settings.png')}/>
-            <Text>Profile</Text>
-          </View>
-        </Tabs>
-
         <Text>
           Hello world
         </Text>
@@ -61,12 +28,6 @@ const style = StyleSheet.create({
     backgroundColor: '#EFF3F6',
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  tabItem: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 });
 
