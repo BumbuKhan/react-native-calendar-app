4108cc9128666e5b3c36069a1409503536ee3f63
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 15:57:11 +0400

    Refactoring - inline styles for tab items have been replaced with classes

--- App.js
+++ App.js
@@ -17,27 +17,27 @@ class App extends Component {
             style={{backgroundColor: 'white'}}
             onSelect={el => this.setState({curPage: el.props.name})}
         >
-          <View name='first' style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <View name='first' style={style.tabItem}>
             <Image source={require('./img/home.png')}/>
             <Text>Home</Text>
           </View>
 
-          <View name='second' style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <View name='second' style={style.tabItem}>
             <Image source={require('./img/users.png')}/>
             <Text>Contacts</Text>
           </View>
 
-          <View name='third' style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <View name='third' style={style.tabItem}>
             <Image source={require('./img/calendar.png')}/>
             <Text>Calendar</Text>
           </View>
 
-          <View name='third' style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <View name='third' style={style.tabItem}>
             <Image source={require('./img/chat.png')}/>
             <Text>Inbox</Text>
           </View>
 
-          <View name='third' style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <View name='third' style={style.tabItem}>
             <Image source={require('./img/settings.png')}/>
             <Text>Profile</Text>
           </View>
@@ -59,6 +59,12 @@ const style = StyleSheet.create({
     backgroundColor: '#EFF3F6',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  tabItem: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
