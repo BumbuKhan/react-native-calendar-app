fb9c8696060b76166283b8fead0ec328fe955a0d
BumbuKhan <qurban.qurbanov93@gmail.com>
2018-03-22 15:22:15 +0400

    Added tabs, with no icons so far

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Tabs from 'react-native-tabs'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {curPage: 'third'}
+  }
+
   render() {
     return (
       <View style={style.container}>
-        <Text>Hello World</Text>
+        <Tabs
+            selected={this.state.curPage}
+            style={{backgroundColor: 'white'}}
+            selectedStyle={{color: '#8CC540'}}
+            onSelect={el => this.setState({curPage: el.props.name})}
+        >
+          <Text name='first'>Home</Text>
+          <Text name='second'>Contacts</Text>
+          <Text name='third'>Calendar</Text>
+          <Text name='forth'>Inbox</Text>
+          <Text name='fifth'>Profile</Text>
+        </Tabs>
       </View>
     );
   }
